da545d676fc9562117ab0f973f2b1efe6786a21d
Matteo Pagani <info@qmatteoq.com>
2015-05-05 22:29:30 +0200

    First commit of the Windows 10 app

---
+++ Marvelpedia.Windows10/Marvelpedia.Windows10/Views/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Marvelpedia.Windows10.Helpers;
+using MarvelPortable;
+using MarvelPortable.Model;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Marvelpedia.Windows10.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            IsLoading.IsActive = true;
+
+            MarvelClient client = new MarvelClient(Constants.ApiKey, Constants.PrivateKey);
+            CharacterResponse response = await client.GetCharactersForSeriesAsync(1067);
+            IEnumerable<Character> filteredList = response.Results.Where(x => x.Thumbnail != null);
+            ObservableCollection<Character> characters = new ObservableCollection<Character>(filteredList);
+            CharactersView.ItemsSource = characters;
+
+            IsLoading.IsActive = false;
+        }
+    }
+}
